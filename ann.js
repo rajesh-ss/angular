82fb68dd969041cd7090036af4862780a1e2fa06
rajesh-ss <88728732+rajesh-ss@users.noreply.github.com>
2022-02-18 15:16:19 +0530

    Add files via upload

---
+++ ann.js
@@ -0,0 +1,15 @@
+
+// Create AngularJS application
+var app = angular.module('myapp',[]);
+// Create Controller with name mainCtrl
+app.controller('myctrl', function($scope,$http){
+$scope.rowlimit=4;
+$scope.today = new Date();
+$http.get('http://localhost:3000/dis')
+.success(function(response)
+{
+//emp=JSON.parse(response.instructor)
+
+});
+});
+
